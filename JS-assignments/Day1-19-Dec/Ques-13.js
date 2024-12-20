f131c02947f960396c4d6744187310368be499e1
Rahul <yashpassi653@gmail.com>
2024-12-20 18:49:49 +0530

    'new assignemt uploded

--- JS-assignments/Day1-19-Dec/Ques-13.js
+++ JS-assignments/Day1-19-Dec/Ques-13.js
@@ -1,1 +1,7 @@
-hy
+let budget = { income: 1000, expenses: 700 };
+budget.savings = budget.income - budget.expenses;
+budget.savings = 1200;
+
+budget.savings = budget.income - budget.expenses;
+
+console.log(budget);
